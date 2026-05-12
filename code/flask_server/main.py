15c71e2dbc3a6178512935e6acd2730618436ee0
ErPollo07 <nicola.tosatti@iisviolamarchesini.edu.it>
2026-05-12 16:56:23 +0200

    Add the robot 3 blueprint

--- code/flask_server/main.py
+++ code/flask_server/main.py
@@ -1,17 +1,16 @@
 from flask import Flask, jsonify, render_template
 from server_log import _log
 
-# Usare pc 10 e 12
-
 # Import blueprints
-from blueprints import robot_bp, robot_1_bp, robot_2_bp
+from blueprints import robot_bp, robot_1_bp, robot_2_bp, robot_3_bp
+
 
 app = Flask(__name__)
-
 
 app.register_blueprint(robot_bp.bp)
 app.register_blueprint(robot_1_bp.bp)
 app.register_blueprint(robot_2_bp.bp)
+app.register_blueprint(robot_3_bp.bp)
 
 
 @app.route('/')
@@ -25,5 +24,5 @@ def status():
 
 
 if __name__ == '__main__':
-    # run the app on http://127.0.0.10:8080/
-    app.run(host='10.33.77.20', port=8080, debug=True)
+    # Usare i pc 10 e 12
+    app.run(host='10.33.77.100', port=8080, debug=True)
