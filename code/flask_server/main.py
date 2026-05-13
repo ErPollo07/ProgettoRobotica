b6fdb5207a8c7b0d9f70840f8f8635770bd7d2ea
ErPollo07 <nicola.tosatti@iisviolamarchesini.edu.it>
2026-05-13 15:08:28 +0200

    Create the html page for the server

--- code/flask_server/main.py
+++ code/flask_server/main.py
@@ -2,15 +2,15 @@ from flask import Flask, jsonify, render_template
 from server_log import _log
 
 # Import blueprints
-from blueprints import robot_bp, robot_1_bp, robot_2_bp, robot_3_bp
+#from blueprints import robot_bp, robot_1_bp, robot_2_bp, robot_3_bp
 
 
 app = Flask(__name__)
 
-app.register_blueprint(robot_bp.bp)
-app.register_blueprint(robot_1_bp.bp)
-app.register_blueprint(robot_2_bp.bp)
-app.register_blueprint(robot_3_bp.bp)
+#app.register_blueprint(robot_bp.bp)
+#app.register_blueprint(robot_1_bp.bp)
+#app.register_blueprint(robot_2_bp.bp)
+#app.register_blueprint(robot_3_bp.bp)
 
 
 @app.route('/')
@@ -25,4 +25,4 @@ def status():
 
 if __name__ == '__main__':
     # Usare i pc 10 e 12
-    app.run(host='10.33.77.100', port=8080, debug=True)
+    app.run(host='10.33.77.20', port=8080, debug=True)
